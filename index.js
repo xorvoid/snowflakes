0105b6e7b28a0aa550ee51aaee39e1ab69356d54
xorvoid <xorvoid@gmail.com>
2024-12-25 12:49:04 +0100

    tweak colors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var canvas_width = 700;
 var canvas_height = 700;
 var snowflake_start = canvas_height/2+10;
 var snowflake_end = -canvas_height/2-10;
+var background_color = '#06224a';
+var snowflake_color = ' #7da2ba ';
 var snowflakes_per_second = 3.0;
 var snowflakes = [];
+var snow_level_y = 0;
 var ctx = null;
 const fps = 60;
 
@@ -16,6 +19,8 @@ function run() {
 function render_frame() {
     window.setTimeout(render_frame, 1000 / fps);
     ctx.clearRect(0, 0, canvas_width, canvas_height);
+    ctx.fillStyle = background_color;
+    ctx.fillRect(0, 0, canvas_width, canvas_height);
     update();
     draw_scene();
 }
@@ -119,21 +124,6 @@ function draw_scene() {
     translate(canvas_width/2, canvas_height/2)
     scale(1, -1);
     //draw_axes();
-
-    // var n = 40;
-    // for(var i = 0; i < n; i++) {
-    //     var x = random_range(-canvas_width/2, canvas_width/2);
-    //     var y = random_range(-canvas_height/2, canvas_height/2);
-    //     var sz = random_range(0.5, 3);
-
-    //     // var loc = ctx.save();
-    //     // translate(x, y);
-    //     // scale(sz/10, sz/10);
-    //     // draw_snowflake();
-    //     // ctx.restore(loc);
-    //     var snowflake = new Snowflake(sz/10, [x, y], [0, 0]);
-    //     snowflake.draw();
-    // }
     for (const snowflake of snowflakes) {
         snowflake.draw();
     }
@@ -142,6 +132,7 @@ function draw_scene() {
 
 function draw_snowflake() {
     var width = 12;
+    var color = snowflake_color;
     function draw_leaf() {
         translate(5, 0);
         
@@ -149,7 +140,7 @@ function draw_snowflake() {
         var loc = ctx.save();
         translate(60, 0);
         scale(120, width-6);
-        square('white');
+        square(color);
         ctx.restore(loc);
 
         // First leaf
@@ -158,7 +149,7 @@ function draw_snowflake() {
         rotate(40);
         translate(35, 0)
         scale(70, width-2);
-        square('white');
+        square(color);
         ctx.restore(loc);
 
         var loc = ctx.save();
@@ -166,7 +157,7 @@ function draw_snowflake() {
         rotate(-40);
         translate(35, 0)
         scale(70, width-2);
-        square('white');
+        square(color);
         ctx.restore(loc);
 
         // Second leaf
@@ -175,14 +166,14 @@ function draw_snowflake() {
         rotate(40);
         translate(20, 0);
         scale(40, width-2);
-        square('white');
+        square(color);
         ctx.restore(loc);
         var loc = ctx.save();
         translate(65, 0);
         rotate(-40);
         translate(20, 0);
         scale(40, width-2);
-        square('white');
+        square(color);
         ctx.restore(loc);
 
         // End
@@ -190,7 +181,7 @@ function draw_snowflake() {
         translate(120, 0);
         rotate(45);
         scale(20, 20);
-        square('white');
+        square(color);
         ctx.restore(loc);
     }
 
@@ -215,7 +206,7 @@ function draw_snowflake() {
 
     var loc = ctx.save();
     scale(28, 28);
-    square('white');
+    square(color);
     ctx.restore(loc);
 
     //ctx.restore(loc_top);
